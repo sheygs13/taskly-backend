5c2c189443bc4f65a2a339736d0214c6163c7f5d
Olusegun <segun.ekoh@gmail.com>
2021-11-14 09:18:44 +0100

    ch: refactor to async-await syntax

--- src/db/db.js
+++ src/db/db.js
@@ -4,19 +4,14 @@ const BASE_URL = "mongodb://127.0.0.1:27017";
 
 const DB_NAME = "taskly-api";
 
-(() => {
+(async () => {
       const options = {
             useNewUrlParser: true,
             useUnifiedTopology: true,
       };
-      mongoose
-            .connect(`${BASE_URL}/${DB_NAME}`, options)
-            .then((result) => {
-                  console.log(result);
-            })
-            .catch(({ message }) => {
-                  console.error(message);
-            });
+      try {
+            await mongoose.connect(`${BASE_URL}/${DB_NAME}`, options);
+      } catch ({ message }) {
+            console.log(message);
+      }
 })();
-
-
