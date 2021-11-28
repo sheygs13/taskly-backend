218e582c196adb32aeb7728087c92d8801340538
Olusegun <segun.ekoh@gmail.com>
2021-11-28 10:44:43 +0100

    changes to mongoose connection

--- src/db/db.js
+++ src/db/db.js
@@ -1,21 +1,20 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const BASE_URL = "mongodb://127.0.0.1:27017";
+const BASE_URL = 'mongodb://127.0.0.1:27017';
 
-const DB_NAME = "taskly-backend";
+const DB_NAME = 'taskly-backend';
 
-const Helpers = require("../helpers/helpers");
+const Helpers = require('../helpers/helpers');
 
 (async (req, res) => {
-      const options = {
-            useNewUrlParser: true,
-            useCreateIndex: true,
-            useFindAndModify: false,
-      };
-      try {
-            await mongoose.connect(`${BASE_URL}/${DB_NAME}`, options);
-
-      } catch ({ message }) {
-            return Helpers.handleErrorResponse(res, 500, message);
-      }
+        const options = {
+                useNewUrlParser: true,
+                //useCreateIndex: true,
+                //useFindAndModify: false,
+        };
+        try {
+                await mongoose.connect(`${BASE_URL}/${DB_NAME}`, options);
+        } catch ({ message }) {
+                return Helpers.handleErrorResponse(res, 500, message);
+        }
 })();
--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,90 +1,90 @@
-const ALLOWED_USER_UPDATES = ["email", "password"];
+const ALLOWED_USER_UPDATES = ['email', 'password'];
 
-const ALLOWED_TASK_UPDATES = ["description", "completed"];
+const ALLOWED_TASK_UPDATES = ['description', 'completed'];
 
 const trimPublicProfile = ({ _id, email, name, createdAt, updatedAt }) => ({
-      _id,
-      email,
-      name,
-      createdAt,
-      updatedAt,
+        _id,
+        email,
+        name,
+        createdAt,
+        updatedAt,
 });
 
 const handleUserUpdate = (update) => ALLOWED_USER_UPDATES.includes(update);
 
 const handleTaskUpdate = (update) => ALLOWED_TASK_UPDATES.includes(update);
 
-const allowedUpdates = (req = {}, updateType = "") => {
-      const clientUpdates = Object.keys(req);
+const allowedUpdates = (req = {}, updateType = '') => {
+        const clientUpdates = Object.keys(req);
 
-      let isValid = false;
+        let isValid = false;
 
-      if (updateType === "user") {
-            isValid = clientUpdates.every(handleUserUpdate);
-      }
-      if (updateType === "task") {
-            isValid = clientUpdates.every(handleTaskUpdate);
-      }
+        if (updateType === 'user') {
+                isValid = clientUpdates.every(handleUserUpdate);
+        }
+        if (updateType === 'task') {
+                isValid = clientUpdates.every(handleTaskUpdate);
+        }
 
-      return isValid;
+        return isValid;
 };
 
 const updateRecord = (item = {}, bodyReq = {}) => {
-      const clientUpdates = Object.keys(bodyReq);
-      for (const update of clientUpdates) {
-            item[update] = bodyReq[update];
-      }
+        const clientUpdates = Object.keys(bodyReq);
+        for (const update of clientUpdates) {
+                item[update] = bodyReq[update];
+        }
 };
 
-const removeSessionTokens = (tokens = [], reqToken = "") =>
-      tokens.filter((tokenObj) => tokenObj.token !== reqToken);
+const removeSessionTokens = (tokens = [], reqToken = '') =>
+        tokens.filter((tokenObj) => tokenObj.token !== reqToken);
 
 const handleSuccessResponse = (res, code, data = {}) => {
-      res.status(code).json({
-            status: "success",
-            data,
-      });
+        res.status(code).json({
+                status: 'success',
+                data,
+        });
 };
 
-const handleErrorResponse = (res, code, errorMessage = "") => {
-      res.status(code).json({
-            status: "fail",
-            error: errorMessage,
-      });
+const handleErrorResponse = (res, code, errorMessage = '') => {
+        res.status(code).json({
+                status: 'fail',
+                error: errorMessage,
+        });
 };
 
 const hasBody = (obj = {}) => Object.keys(obj).length;
 
 const returnMessage = (arr) => {
-      return `${arr.length > 1 ? `${arr.length} tasks` : `${arr.length} task`}  found.`;
+        return `${arr.length > 1 ? `${arr.length} tasks` : `${arr.length} task`}  found.`;
 };
 
 const paginateData = (Limit, Skip) => ({
-      limit: +Limit,
-      skip: +Skip,
+        limit: +Limit,
+        skip: +Skip,
 });
 
 const sortOption = (sortBy) => {
-      const sort = {};
-      let parts = [];
-      if (sortBy) {
-            parts = sortBy.split(":");
-            sort[parts[0]] = parts[1] === "desc" ? -1 : 1;
-      }
-      return {
-            ...sort,
-      };
+        const sort = {};
+        let parts = [];
+        if (sortBy) {
+                parts = sortBy.split(':');
+                sort[parts[0]] = parts[1] === 'desc' ? -1 : 1;
+        }
+        return {
+                ...sort,
+        };
 };
 
 module.exports = {
-      trimPublicProfile,
-      handleErrorResponse,
-      handleSuccessResponse,
-      allowedUpdates,
-      updateRecord,
-      removeSessionTokens,
-      hasBody,
-      returnMessage,
-      paginateData,
-      sortOption,
+        trimPublicProfile,
+        handleErrorResponse,
+        handleSuccessResponse,
+        allowedUpdates,
+        updateRecord,
+        removeSessionTokens,
+        hasBody,
+        returnMessage,
+        paginateData,
+        sortOption,
 };
