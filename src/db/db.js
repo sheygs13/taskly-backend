ab6980db7d8e04c578bafb7deee8b05c6f7b0be8
Olusegun <segun.ekoh@gmail.com>
2021-12-22 03:39:32 +0100

    change

--- src/db/db.js
+++ src/db/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 (async () => {
         try {
-                await mongoose.connect(`${process.env.DB_CONN_URL_TESTING}`, {
+                await mongoose.connect(`${process.env.DB_CONN_URL}`, {
                         useNewUrlParser: true,
                 });
         } catch ({ message }) {
